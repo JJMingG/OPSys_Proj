e03ff9a3ec81f041fe7bc6f9b08f2b2563d20953
Evan Schwalb <evanschwalb@gmail.com>
2018-01-26 08:02:13 -0500

    Add files via upload

--- Project1_COP4016.c
+++ Project1_COP4016.c
@@ -7,23 +7,42 @@ main()
 {
   char *name;
   char input[15];
+  char cwd[200];
   name=(char *)malloc(10*sizeof(char));
   struct utsname uData;
   cuserid(name);
   uname(&uData);
+//  getcwd(direc, sizeof(direc));
   printf("Bash Shell emulation initiated\n");
   printf ("%s", name);
   printf("@");
   printf("%s", uData.sysname);
-  printf(" :: =>");
+  printf(" :: ");
+
+  if (getcwd(cwd, sizeof(cwd)) == NULL){
+     perror("getcwd() error");
+   }
+   else{
+     printf("%s", cwd);
+ }
+  printf(" =>");
 while (strcmp(input, "exit") != 0){
+//  getcwd(direc, sizeof(direc));
   cuserid(name);
 fgets(input, 15, stdin);
 printf ("%s", name);
 printf("@");
 uname(&uData);
 printf("%s", uData.sysname);
-printf(" :: =>");
+printf(" :: ");
+if (getcwd(cwd, sizeof(cwd)) == NULL){
+   perror("getcwd() error");
+ }
+ else{
+   printf( "%s", cwd);
+//fprintf(stdout, "%s", direc);
+        }
+printf(" =>");
+    }
 
-}
 }
