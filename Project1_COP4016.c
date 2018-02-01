e8e1097fce99d9e0520ac706c9d9c996cdd2ef21
schwalbevans <evanschwalb@gmail.com>
2018-01-31 23:18:28 -0500

    Updated Parsing and added comments

--- Project1_COP4016.c
+++ Project1_COP4016.c
@@ -8,12 +8,12 @@ void ParseIt(char* input);
 main()
 {
   char *name;
-  char input[26];
-  char cwd[200];
+  char input[26]; // array for user input
+  char cwd[200]; // array for hold current directory
   name=(char *)malloc(10*sizeof(char));
   struct utsname uData;
-  cuserid(name);
-  uname(&uData);
+  cuserid(name); // get current user
+  uname(&uData); //get current machine name
 //  getcwd(direc, sizeof(direc));
   printf("Bash Shell emulation initiated\n");
   printf ("%s", name);
@@ -25,15 +25,15 @@ main()
      perror("getcwd() error");
    }
    else{
-     printf("%s", cwd);
+     printf("%s", cwd); // get current working directory
  }
   printf(" =>");
-while (strcmp(input, "exit") != 0){
-//  getcwd(direc, sizeof(direc));
+while (strcmp(input, "exit") != 0){ //checks for exit cmd
+//  getcwd(direc, sizeof(direc)); // ignore for now
   cuserid(name);
 fgets(input, 25, stdin);
 printf("%s", input);
-ParseIt(input);
+ParseIt(input); // calls parsing function seen below
 printf ("%s", name);
 printf("@");
 uname(&uData);
@@ -52,13 +52,24 @@ printf(" =>");
 }
 
 void ParseIt(char* input){
+  char cmdarray[35];
+  int cmd_array_counter = 0;
 for (int i = 0; i < sizeof(input); i++){
   if (input[i] == '&'){
-printf("ignore &");
-    }
-    printf("hello world");
+    i++;
+  }
+else if(input[i] != '|' ||input[i] != '<' || input[i] != '>' || input[i] != '&'){ //checks that its a command and not a special character
+while(input[i] != ' '){
+char cmdarray[cmd_array_counter] = input[i]; // throw cmd into array
+cmd_array_counter++;
+if (input[i + 1]  == '|' ){
 
 
+
+}
+}
+
+}
   }
 
 
